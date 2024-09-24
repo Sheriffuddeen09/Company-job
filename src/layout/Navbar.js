5bb5cf718d488a11c2cd5c77e172f7b32625efea
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-09-24 18:13:44 +0100

    commit

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
 
     return (
 
-        <header className=" fixed bg-black w-full z-10  shadow-2xl border-b-1 border-l-0 border-r-0 border-t-0   border-white border -py-10 sm:px-0 ">
+        <header className=" fixed bg-black w-full z-10  shadow-2xl sm:px-0 ">
             <nav className='flex flex-row justify-between items-center mx-5'>
               <div className='inline-flex items-center gap-2'>
                 <img src={Image} alt="logo" width={50} height={70} className='w-32'/>
@@ -112,23 +112,40 @@ function Navbar() {
             </nav>
             <div  onClick={handlemenu}  className={` fixed top-0 right-0 w-full h-full bg-menu ${menu ? "block" :"hidden"}`}> 
 
-<div className='gap-2 flex-col flex bg-black w-64 fixed right-0 p-4 text-black h-full'>
+<div className='gap-2 flex-col flex bg-black w-64 fixed right-0 p-4 text-black h-full text-end'>
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" onClick={handleHide} stroke="currentColor" class="size-6 cursor-pointer text-white mt-10 relative left-48 bottom-4">
 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
 </svg>
 
-<a href='/' className="text-white font-bold flex flex-row justify-between text-sm  rounded-2xl transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer p-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-</svg>
-Logistic 
-</a>
-<hr  className='bg-black w-full'/>
-<a href='/' className="text-white font-bold  text-sm rounded-2xl transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer p-2 flex flex-row justify-between"> <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-4">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-</svg>
-Technology
-</a>
-<hr  className='bg-black'/>
+<Link to={'/'} className={`text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer `}>MISSION
+                <div className={`transition-all duration-500 ease-in-out hover:bg-[#626267] ${homepage === '/' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/' ? 'hidden' : 'block'}`}/>
+                <Link to={'/launch'} className="text-white text-sm text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer">LAUNCHES
+                <div className={`${homepage === '/launch' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/launch' ? 'hidden' : 'block'}`}/>
+                <Link to={'/career'} className="text-white text-sm text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer">CAREERS
+                <div className={`${homepage === '/career' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/career' ? 'hidden' : 'block'}`}/>
+                <Link to={'/update'} className="text-white text-sm text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer">UPDATES
+                <div className={`${homepage === '/update' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/update' ? 'hidden' : 'block'}`}/>
+                <Link to={'/shop'} className="text-white text-sm text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer">SHOP
+                <div className={`${homepage === '/shop' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/shop' ? 'hidden' : 'block'}`}/>
+                <Link to={'/log'} className="text-white text-sm text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer">LOGISTICS
+                <div className={`${homepage === '/log' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/log' ? 'hidden' : 'block'}`}/>
+                <Link to={'/tech'} className="text-white text-sm text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out hover:bg-[#626267] cursor-pointer">TECHNOLOGY
+                <div className={`${homepage === '/tech' ? 'home' : 'none'}`}> </div>
+                </Link>
+                <hr className={` h-0.5 ${homepage === '/tech' ? 'hidden' : 'block'}`}/>
+               
 </div>
 </div>
         </header>
