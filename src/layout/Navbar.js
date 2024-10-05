51f51a306499a3d10e11fab29cef1968700803f3
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-10-05 13:28:54 -0700

    commit

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
 
     return (
 
-        <header className=" bg-black w-full z-10  shadow-2xl p-3 sm:p-6 ">
+        <header className=" bg-black w-full z-10  shadow-2xl px-3 py-4 sm:p-6 ">
             <nav className='flex flex-row justify-between items-center mx-2 lg:mx-7'>
               <div className='inline-flex items-center gap-2'>
                 <Link to={'/'}>
@@ -59,7 +59,7 @@ function Navbar() {
             <div onClick={handleHide} className={`block z-10 showMenu sm:hidden fixed top-0 left-0 w-full h-full bg-menu ${hide ? 'block' :'hide'}`}> 
             
             <section className='gap-2 flex-col flex bg-black w-72 fixed right-0 p-4 text-black h-full text-end'>
-            <div className='flex-row justify-between flex items-center gap-2'>
+            <div className='flex-row justify-between flex mt-2 items-center gap-2'>
             <Link to={'/'}>
             <img src={logos} alt='logo' width={140} height={80}/>
             </Link>
@@ -69,7 +69,7 @@ function Navbar() {
             class="cursor-pointer showrotate transition-all duration-500 before:absolute before:h-0.5 before:w-4 before:-translate-x-4 before:translate-y-2 before:rounded before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-0.5 after:w-4 after:-translate-x-4 after:translate-y-2 after:rotate-75 before:rotate-90 after:rounded after:bg-white after:transition-all after:duration-500 after:content-[''] -translate-y-1 -translate-x-2 ">
           </div>
           </button>
-
+          </div>
             <li className='-mb-5'>
 
                 <Link to={'/about'} className={`text-white text-sm  rounded-xl p-1 transition-all duration-500 ease-in-out hover:bg-[#000000b3]  hover:text-gray-600 cursor-pointer`}><p className='-translate-x-2 -mb-3'>ABOUT</p>
@@ -81,7 +81,6 @@ function Navbar() {
                 </Link>
                 <li className={` bg-line w-60 mx-auto `}> </li>
                 </li>             
-                </div>
                 <li className='-mb-5'>
                 <Link to={'/career'} className={`text-white text-sm  rounded-xl p-2 transition-all duration-500 ease-in-out cursor-pointer home`}><p className='-translate-x-2 -mb-3'>CAREERS</p>
                 </Link>
@@ -104,13 +103,19 @@ function Navbar() {
             <div  onClick={handlemenu}  className={`z-10 transition-all duration-3000 ease-in-out fixed top-0 right-0 w-full h-full bg-menu ${menu ? "blocked" :"hide"}`}> 
 
                 <section className='gap-2 flex-col transition-all duration-2000 ease-in-out flex bg-black w-80 fixed right-0 p-4 text-black h-full text-end '>
-                <button className='relative top-4 left-32 mb-14' onClick={handlemenu} >
-                <div
-                            class="showrotate transition-all duration-500 before:absolute before:h-0.5 before:w-4 cursor-pointer before:-translate-x-5 before:translate-y-7 before:rounded before:rotate-45  before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-0.5 after:w-4 after:-translate-x-5 after:translate-y-7 after:-rotate-45 after:rounded after:bg-white after:transition-all after:duration-500 after:content-[''] rotate-[720deg] transition-all duration-500">
-                          </div>
-                </button>
+                <div className='flex-row justify-between flex items-center mt-4 gap-2'>
+                <Link to={'/'}>
+                <img src={logos} alt='logo' width={140} height={80}/>
+                </Link>
+                <button className='rotate-45 translate-y-2' onClick={handleHide}>
 
-           
+                <div
+                class="cursor-pointer showrotate transition-all duration-500 before:absolute before:h-0.5 before:w-4 before:-translate-x-4 before:translate-y-2 before:rounded before:bg-white before:transition-all before:duration-500 before:content-[''] after:absolute after:h-0.5 after:w-4 after:-translate-x-4 after:translate-y-2 after:rotate-75 before:rotate-90 after:rounded after:bg-white after:transition-all after:duration-500 after:content-[''] -translate-y-1 -translate-x-2 ">
+              </div>
+              </button>
+              </div>
+
+              
                 <li className='-mb-5'>
                 <Link to={'/about'} className={`text-white text-sm  rounded-xl p-1 transition-all duration-500 ease-in-out hover:bg-[#000000b3]  hover:text-gray-600 cursor-pointer`}><p className='-translate-x-5 -mb-3'>ABOUT</p>
                 </Link>
