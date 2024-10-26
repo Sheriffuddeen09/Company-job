9f5105a16c6681f26c893c117d1643da8079911e
Sheriffuddeen09 <odukoyasheriff@gmail.com>
2024-10-26 21:52:08 +0100

    commit

--- src/logistic/LogisticVideo.js
+++ src/logistic/LogisticVideo.js
@@ -5,7 +5,7 @@ import logo from '../image/ECOREME White PNG Logo.png'
 function LogisticVideo (){
 
     return (
-        <div >
+        <div>
             <div className=''>               
                 <Video autoPlay
                     poster={logo}
