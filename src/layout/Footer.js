253194ecea0d1a4a823ae89e33454dda30dcd265
musmax <musaojo1710@yahoo.com>
2025-01-05 10:07:33 +0100

    chore: made ui changes

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,15 +1,15 @@
 
-const Footers = () =>{
+const Footers = () => {
 
     return (
         <div className="bg-black p-4 text-center fixed w-full z-0 bottom-0">
-         <div className="inline-flex -mt-80 text-white text-center gap-5" style={{fontSize:"13px"}}>
-        <p>&copy;2024 ECOREME TECHNOLOGIES</p>    
+            <div className="inline-flex -mt-80 text-white text-center gap-5" style={{ fontSize: "13px" }}>
+                <p>&copy;{new Date().getFullYear()} ECOREME TECHNOLOGIES</p>
+            </div>
         </div>
-        </div>
-    
-    
+
+
     )
-    }
-    export default Footers
+}
+export default Footers
 
